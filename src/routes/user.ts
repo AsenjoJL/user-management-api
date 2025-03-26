b0f67c29df7c5b5150c8013c7e3bff9df5d59382
keynard <keynardamacna@gmail.com>
2025-03-26 13:18:30 +0800

    amacna deletion

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,30 +1,3 @@
-import * as express from "express";
-import { AppDataSource } from "../config/database";
-import { User } from "../entities/User";
-
-const router = express.Router();
-
-//Create 
-// 📌 Create a new user (POST request)
-router.post("/", async (req, res) => {
-  console.log("🔍 Creating a new user...");
-  const userRepository = AppDataSource.getRepository(User);
-  const { name, username, email, password } = req.body;
-
-  if (!name || !username || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-
-  try {
-    const newUser = userRepository.create({ name, username, email, password });
-    await userRepository.save(newUser);
-    console.log("✅ User created:", newUser);
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error("❌ Error creating user:", error);
-    res.status(500).json({ error: "Error creating user" });
-  }
-});
 
 // 📌 Delete a user by ID (DELETE request)
 router.delete("/:id", async (req, res) => {
@@ -49,4 +22,3 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-export default router;
